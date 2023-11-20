4b8ba71ddaf57f65d3756d5ae64615070e99f76d
sota2800 <141693046+sota2800@users.noreply.github.com>
2023-11-20 21:41:13 +0900

    Add files via upload

---
+++ select/select_idl_example.py
@@ -0,0 +1,95 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- Python -*-
+
+"""
+ @file select_idl_example.py
+ @brief Python example implementations generated from select.idl
+ @date $Date$
+
+
+"""
+
+import omniORB
+from omniORB import CORBA, PortableServer
+import Library, Library__POA
+
+class datacode:
+    def __init__(self,state,recogdata,command,phase):
+        self.state=state
+        self.recogdata=recogdata
+        self.command=command
+        self.phase=phase
+
+class selectdata_i (Library__POA.selectdata):
+    """
+    @class selectdata_i
+    Example class implementing IDL interface Library.selectdata
+    """
+
+    def __init__(self):
+        """
+        @brief standard constructor
+        Initialise member variables here
+        """
+        self.state="NUM"
+        self.recogdata="ぴよ"
+        self.command="NUM"
+        self.phase="NUM"
+        #pass
+
+    def getdata(self):
+        data=datacode(self.state,self.recogdata,self.command,self.phase)
+        return data
+    def setdata(self):
+        data=datacode(self.state1,self.recogdata1,self.command1,self.phase1)
+        return data
+
+    # Dataset setresult(in string data)
+    def setresult(self, data):
+        data1=datacode(self.state,self.recogdata,self.command,self.phase)
+
+        #raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+        return data1
+
+    # Dataset select(in Dataset data)
+    def select(self, data):
+        self.state=data.state
+        self.recogdata=data.recogdata
+        self.command=data.command
+        self.phase=data.phase
+        #raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+        return data
+
+
+if __name__ == "__main__":
+    import sys
+    
+    # Initialise the ORB
+    orb = CORBA.ORB_init(sys.argv)
+    
+    # As an example, we activate an object in the Root POA
+    poa = orb.resolve_initial_references("RootPOA")
+
+    # Create an instance of a servant class
+    servant = selectdata_i()
+
+    # Activate it in the Root POA
+    poa.activate_object(servant)
+
+    # Get the object reference to the object
+    objref = servant._this()
+    
+    # Print a stringified IOR for it
+    print(orb.object_to_string(objref))
+
+    # Activate the Root POA's manager
+    poa._get_the_POAManager().activate()
+
+    # Run the ORB, blocking this thread
+    orb.run()
+
---
+++ select/test/selectTest.py
@@ -0,0 +1,298 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- Python -*-
+
+# <rtc-template block="description">
+"""
+ @file selectTest.py
+ @brief ModuleDescription
+ @date $Date$
+
+
+"""
+# </rtc-template>
+
+from __future__ import print_function
+import sys
+import time
+sys.path.append(".")
+
+# Import RTM module
+import RTC
+import OpenRTM_aist
+
+import select_idl
+
+# Import Service implementation class
+# <rtc-template block="service_impl">
+
+import select
+
+# </rtc-template>
+
+# Import Service stub modules
+# <rtc-template block="consumer_import">
+# </rtc-template>
+
+
+# This module's spesification
+# <rtc-template block="module_spec">
+selecttest_spec = ["implementation_id", "selectTest", 
+         "type_name",         "selectTest", 
+         "description",       "ModuleDescription", 
+         "version",           "1.0.0", 
+         "vendor",            "VenderName", 
+         "category",          "Category", 
+         "activity_type",     "STATIC", 
+         "max_instance",      "1", 
+         "language",          "Python", 
+         "lang_type",         "SCRIPT",
+         ""]
+# </rtc-template>
+
+# <rtc-template block="component_description">
+##
+# @class selectTest
+# @brief ModuleDescription
+# 
+# 
+# </rtc-template>
+class selectTest(OpenRTM_aist.DataFlowComponentBase):
+    
+    ##
+    # @brief constructor
+    # @param manager Maneger Object
+    # 
+    def __init__(self, manager):
+        OpenRTM_aist.DataFlowComponentBase.__init__(self, manager)
+
+        self._d_controlesota = OpenRTM_aist.instantiateDataType(RTC.TimedString)
+        """
+        """
+        self._controlesotaIn = OpenRTM_aist.InPort("controlesota", self._d_controlesota)
+
+        """
+        """
+        self._selectproviderPort = OpenRTM_aist.CorbaPort("selectprovider")
+
+        
+
+        """
+        """
+        self._selectprovider = OpenRTM_aist.CorbaConsumer(interfaceType=Library.selectdata)
+
+        # initialize of configuration-data.
+        # <rtc-template block="init_conf_param">
+        
+        # </rtc-template>
+
+
+         
+    ##
+    #
+    # The initialize action (on CREATED->ALIVE transition)
+    # 
+    # @return RTC::ReturnCode_t
+    # 
+    #
+    def onInitialize(self):
+        # Bind variables and configuration variable
+        
+        # Set InPort buffers
+        self.addInPort("controlesota",self._controlesotaIn)
+        
+        # Set OutPort buffers
+        
+        # Set service provider to Ports
+        
+        # Set service consumers to Ports
+        self._selectproviderPort.registerConsumer("selectdata", "Library::selectdata", self._selectprovider)
+        
+        # Set CORBA Service Ports
+        self.addPort(self._selectproviderPort)
+        
+        return RTC.RTC_OK
+    
+    ###
+    ## 
+    ## The finalize action (on ALIVE->END transition)
+    ## 
+    ## @return RTC::ReturnCode_t
+    #
+    ## 
+    #def onFinalize(self):
+    #
+    #    return RTC.RTC_OK
+    
+    #    ##
+    ##
+    ## The startup action when ExecutionContext startup
+    ## 
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onStartup(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The shutdown action when ExecutionContext stop
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onShutdown(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The activated action (Active state entry action)
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ## 
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onActivated(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    #    ##
+    ##
+    ## The deactivated action (Active state exit action)
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onDeactivated(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ##
+    #
+    # The execution action that is invoked periodically
+    #
+    # @param ec_id target ExecutionContext Id
+    #
+    # @return RTC::ReturnCode_t
+    #
+    #
+    def onExecute(self, ec_id):
+    
+        return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The aborting action when main logic error occurred.
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    #    #
+    ##
+    #def onAborting(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The error action in ERROR state
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onError(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The reset action that is invoked resetting
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onReset(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The state update action that is invoked after onExecute() action
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+
+    ##
+    #def onStateUpdate(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    ###
+    ##
+    ## The action that is invoked when execution context's rate is changed
+    ##
+    ## @param ec_id target ExecutionContext Id
+    ##
+    ## @return RTC::ReturnCode_t
+    ##
+    ##
+    #def onRateChanged(self, ec_id):
+    #
+    #    return RTC.RTC_OK
+    
+    def runTest(self):
+        return True
+
+def RunTest():
+    manager = OpenRTM_aist.Manager.instance()
+    comp = manager.getComponent("selectTest0")
+    if comp is None:
+        print('Component get failed.', file=sys.stderr)
+        return False
+    return comp.runTest()
+
+def selectTestInit(manager):
+    profile = OpenRTM_aist.Properties(defaults_str=selecttest_spec)
+    manager.registerFactory(profile,
+                            selectTest,
+                            OpenRTM_aist.Delete)
+
+def MyModuleInit(manager):
+    selectTestInit(manager)
+    select.selectInit(manager)
+
+    # Create a component
+    comp = manager.createComponent("selectTest")
+
+def main():
+    mgr = OpenRTM_aist.Manager.init(sys.argv)
+    mgr.setModuleInitProc(MyModuleInit)
+    mgr.activateManager()
+    mgr.runManager(True)
+
+    ret = RunTest()
+    mgr.shutdown()
+
+    if ret:
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
+
